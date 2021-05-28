9b0e73777cb99cd53b00f7ea06e126b67c563bdf
Scala Steward <me@scala-steward.org>
2021-05-28 12:42:19 +0200

    Update scala-compiler, scala-library, ... to 2.12.14

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -4,7 +4,7 @@ object ScalaVersions {
   val crossScala213 = Seq("2.13.4", "2.13.5", "2.13.6")
 
   val scala211: String = "2.11.12"
-  val scala212: String = "2.12.13"
+  val scala212: String = "2.12.14"
   val scala213: String = crossScala213.last
 
   val sbt10Version: String      = "1.1.6" // minimum version
